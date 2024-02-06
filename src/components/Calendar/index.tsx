12711a61d4599f46d5ddeaa1a926ae1980d80ad1
José Vitor <jose-vitor-7@hotmail.com>
2024-02-05 21:04:29 -0300

    Calendar & availability - showing available times

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -95,8 +95,6 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
     return calendarWeeks
   }, [currentDate])
 
-  console.log(calendarWeeks)
-
   return (
     <CalendarContainer>
       <CalendarHeader>
