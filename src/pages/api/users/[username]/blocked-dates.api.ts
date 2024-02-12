a1527828d7c6a53c976728deb930ed03a03cd62b
José Vitor <jose-vitor-7@hotmail.com>
2024-02-11 23:04:46 -0300

    feat: change database to postgres

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -39,21 +39,38 @@ export default async function handle(
       ),
   )
 
+  // const blockedDatesRaw = await prisma.$queryRaw<Array<{ d: number }>>`
+  //   select
+  //       extract(day from s.date) as d,
+  //       count(s.date) as amount,
+  //       ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60) as s
+  //     from schedulings as s
+  //     left join user_time_intervals as uti
+  //       on uti.week_day = weekday(date_add(s.date, interval 1 day))
+  //     where s.user_id = ${user.id}
+  //       and date_format(s.date, "%Y-%m") = ${`${year}-${month}`}
+  //     group by
+  //       extract(day from s.date),
+  //       ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60)
+  //     having
+  //       amount >= s
+  // `
+
   const blockedDatesRaw = await prisma.$queryRaw<Array<{ d: number }>>`
     select
-        extract(day from s.date) as d,
-        count(s.date) as amount,
-        ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60) as s
-      from schedulings as s
-      left join user_time_intervals as uti
-        on uti.week_day = weekday(date_add(s.date, interval 1 day))
-      where s.user_id = ${user.id}
-        and date_format(s.date, "%Y-%m") = ${`${year}-${month}`}
-      group by
-        extract(day from s.date),
-        ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60)
-      having
-        amount >= s
+      extract(day from s.date) as d,
+      count(s.date),
+      ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60)
+    from schedulings as s
+    left join user_time_intervals uti
+      on uti.week_day = extract(dow from s.date + interval '1 day')
+    where s.user_id = ${user.id}
+      and extract(year from s.date) = ${year}::int
+      and extract(month from s.date) = ${month}::int
+    group by extract(day from s.date),
+      ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60)
+    having
+      count(s.date) >= ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60);
   `
 
   const blockedDates = blockedDatesRaw.map((item) => item.d)
